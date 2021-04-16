0924eec50ad9d8a3c9ca7cc219e3bb987e445705
Ramil Safin <RamilSafNab1996@gmail.com>
2021-04-16 17:18:55 +0300

    Working solution

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 #include "binary_search_tree.hpp"
@@ -7,8 +8,31 @@
 using namespace std;
 using namespace itis;
 
+static void treeAsVector(int node_id, Node *node, vector<Node *> &tree) {
+
+  if (node == nullptr) {
+    return;
+  }
+
+  const int left_child_id = node_id * 2 + 1;
+
+  if (left_child_id >= tree.size()) {
+    return;
+  }
+
+  tree[left_child_id] = node->left;
+  tree[left_child_id + 1] = node->right;
+
+  treeAsVector(left_child_id, node->left, tree);
+  treeAsVector(left_child_id + 1, node->right, tree);
+}
+
 static vector<Node *> treeAsVector(const BinarySearchTree &tree) {
-  vector<Node *> array_tree;
+  vector<Node *> array_tree(tree.size(), nullptr);
+
+  array_tree[0] = tree.root();
+  treeAsVector(0, tree.root(), array_tree);
+
   return array_tree;
 }
 
@@ -17,21 +41,22 @@ int main(int argc, char **argv) {
   BinarySearchTree tree;
 
   tree.Insert(8);
-  tree.Insert(3);
   tree.Insert(10);
-  tree.Insert(1);
-  tree.Insert(6);
-  tree.Insert(4);
-  tree.Insert(7);
-  tree.Insert(14);
+  tree.Insert(11);
+  tree.Insert(12);
   tree.Insert(13);
 
   tree.Traverse(BreadthFirstTraversalAlgorithm{});
+  cout << endl;
 
   const auto tree_as_vector = treeAsVector(tree);
 
-  for (const auto* node: tree_as_vector) {
-    cout << node->key << '\n';
+  for (const auto *node : tree_as_vector) {
+    if (node != nullptr) {
+      cout << "Node: " << node->key << endl;
+    } else {
+      cout << "Node: nullptr" << endl;
+    }
   }
 
   return 0;
